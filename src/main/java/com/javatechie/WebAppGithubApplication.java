2a77adfa61ca65a2d3af8ee7763fa078b851ea19
Saurabh <saurabh.sb83@gmail.com>
2022-05-27 09:10:30 +0530

    Added the source code

---
+++ src/main/java/com/javatechie/WebAppGithubApplication.java
@@ -0,0 +1,14 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppGithubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebAppGithubApplication.class, args);
+	}
+
+}
